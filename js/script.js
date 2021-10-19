e1e19d6610f04a46cb4823a45d666af279365e23
Ezhoff66 <ezhoff.fomin@yandex.ru>
2021-10-20 04:11:08 +0500

    Все работает, бургер на флексах глючит

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $('.menu__icon').click(function () {
+        $('.menu__icon').toggleClass('menu__icon_active');
+        $('.menu__body').toggleClass('menu__body_active');
+        $('body').toggleClass('lock');
+    })
+    $('.slider__body').slick({
+        arrows:false,
+        dots:false,
+        autoplay:true,
+        autoplaySpeed: 6000,
+        pauseOnDotsHover: true,
+        waitForAnimate: false
+    });
+});
