5234008e59f328cc13686b4b5a8bb6abfe17aa76
Ezhoff66 <ezhoff.fomin@yandex.ru>
2021-10-23 03:24:17 +0500

    Переделал бургер меню, с пк и мобильной версией

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('.menu__icon').click(function () {
-        $('.menu__icon').toggleClass('menu__icon_active');
+        // $('.menu__icon').toggleClass('menu__icon_active');
         $('.menu__body').toggleClass('menu__body_active');
         $('.menu-navigation-container').toggleClass('menu-navigation-container_active');
 
