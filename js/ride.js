98385c6f56eff041166d07b434c0ce8bfd2da43c
Ezhoff66 <ezhoff.fomin@yandex.ru>
2021-10-22 20:37:06 +0500

    Переделал вложенность меню в хедере, нужно еще править. Скопировал главную в "прогулки"

--- js/ride.js
+++ js/ride.js
@@ -1,17 +1,8 @@
 $(document).ready(function () {
     $('.menu__icon').click(function () {
         $('.menu__icon').toggleClass('menu__icon_active');
-        $('.menu__body').toggleClass('menu__body_active');
         $('.menu-navigation-container').toggleClass('menu-navigation-container_active');
 
         $('body').toggleClass('lock');
-    });
-    $('.slider__body').slick({
-        arrows:false,
-        dots:false,
-        autoplay:true,
-        autoplaySpeed: 6000,
-        pauseOnDotsHover: true,
-        waitForAnimate: false
     });
 });
